92f5be825bbd3b077da9a7aa0715634f92f2ce1c
RomChung <thelns@vip.qq.com>
2021-02-03 17:00:32 +0800

    update: option samples

--- code/option.rs
+++ code/option.rs
@@ -4,6 +4,22 @@ fn main() {
     let last_name = Some(format!("Chung"));
     let last_name_none: Option<String> = None;
     // println!("{}", first_name); // Option<String>
+
+    let first_name_string = match first_name {
+        Some(ref name) => name.clone(),
+        None => format!("none"),
+    };
+
+    let last_name_none_string = match last_name_none {
+        Some(ref name) => name.clone(),
+        None => format!("none"),
+    };
+
+    println!("{}", first_name_string);
+    println!("{}", last_name_none_string);
     println!("{}", first_name.unwrap());
-    println!("{}", last_name_none.expect("last_name is missing")); // panic with error
+
+    println!("{}", last_name_none.unwrap_or(format!("default_name")));
+
+    // println!("{}", last_name_none.expect("last_name is missing")); // panic with error
 }
