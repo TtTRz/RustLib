3e4a06058824c7beffaa43df3a101e8e94b953c9
RomChung <thelns@vip.qq.com>
2021-02-04 13:19:51 +0800

    update: lifetime

--- code/lifetime.rs
+++ code/lifetime.rs
@@ -4,6 +4,20 @@ struct Name<'a> {
     last_name: &'a str,
 }
 
+#[derive(Debug)]
+struct Person<'a, 'b> {
+    name: &'a str,
+    address: &'b str,
+}
+
+fn longer<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
+}
+
 fn main() {
     let name = String::from("Rom");
     {
@@ -20,5 +34,16 @@ fn main() {
         last_name: l_n_str,
     };
 
+    {
+        let address = "guangzhou";
+        let person = Person {
+            name: &name,
+            address,
+        };
+        println!("{:?}", person);
+    }
+
     println!("{:?}", person_name);
+
+    println!("{}", longer(f_n_str, l_n_str));
 }
