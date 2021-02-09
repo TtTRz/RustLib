0161b100e8879ac2163bb5cf1409930351e70974
RomChung <thelns@vip.qq.com>
2021-02-09 19:27:51 +0800

    docs: add map

--- code/Branch/src/main.rs
+++ code/Branch/src/main.rs
@@ -1,78 +1,104 @@
 // branch
-
+#[derive(Debug)]
 enum Sex {
-  Male,
-  Female,
+    Male,
+    Female,
+    Other,
 }
 
 struct Person {
-  name: String,
-  sex: Sex,
+    name: String,
+    sex: Sex,
 }
 
 fn main() {
-  // if
-  let mut a = 1;
-  if a == 1 {
-      println!(" a == 1: {}", true);
-  } else {
-      println!(" a == 1: {}", false);
-  }
+    // if
+    let mut a = 1;
+    if a == 1 {
+        println!(" a == 1: {}", true);
+    } else {
+        println!(" a == 1: {}", false);
+    }
 
-  let mut b = false;
-  b = if a == 1 { true } else { false };
-  println!("b: {}", b);
+    let mut b = false;
+    b = if a == 1 { true } else { false };
+    println!("b: {}", b);
 
-  // match
-  let male = Person {
-      name: format!("Rom"),
-      sex: Sex::Male,
-  };
-  let female = Person {
-      name: format!("Yuki"),
-      sex: Sex::Female,
-  };
+    // match
+    let male = Person {
+        name: format!("Rom"),
+        sex: Sex::Male,
+    };
+    let female = Person {
+        name: format!("Yuki"),
+        sex: Sex::Female,
+    };
 
-  match male.sex {
-      Sex::Male => println!("{} is male !", male.name),
-      Sex::Female => println!("{} is female", male.name),
-  }
+    let other = Person {
+        name: "o".into(),
+        sex: Sex::Other,
+    };
 
-  match female.sex {
-      Sex::Male => println!("{} is male !", female.name),
-      Sex::Female => println!("{} is female", female.name),
-  }
+    match male.sex {
+        Sex::Male => println!("{} is male !", male.name),
+        Sex::Female => println!("{} is female", male.name),
+        _ => println!("{} dont know", female.name),
+    }
 
-  // for
-  let mut arr = [1, 2, 3, 4, 5];
+    match female.sex {
+        Sex::Male => println!("{} is male !", female.name),
+        Sex::Female => println!("{} is female", female.name),
+        _ => println!("{} dont know", female.name),
+    }
 
-  for i in &arr {
-      println!("item: {}", i);
-  }
+    match male.sex {
+        Sex::Male | Sex::Female => println!("{} know sex", female.name),
+        _ => println!("{} dont know", female.name),
+    }
 
-  for (index, item) in arr.iter().enumerate() {
-      println!("index: {}, item: {}", index, item);
-  }
+    println!("{:?}", female.sex);
 
-  for (index, item) in arr.iter_mut().enumerate() {
-      *item += index;
-      println!("index: {}, item + index: {}", index, item);
-  }
+    let op: Option<String> = Some("rom".into());
 
-  // while
-  while a != 10 {
-      a += 1;
-      println!("while (a = {})", a);
-  }
+    match op {
+        Some(ref h) => {
+          
+        }
+        None => {}
+    }
 
-  println!("{}", a);
-  // loop
+    println!("{:?}", op);
 
-  loop {
-      a += 1;
-      if a == 20 {
-          print!("{}", a);
-          break;
-      }
-  }
+    // for
+    let mut arr = [1, 2, 3, 4, 5];
+
+    for i in &arr {
+        println!("item: {}", i);
+    }
+
+    for (index, item) in arr.iter().enumerate() {
+        println!("index: {}, item: {}", index, item);
+    }
+
+    for (index, item) in arr.iter_mut().enumerate() {
+        *item += index;
+        println!("index: {}, item + index: {}", index, item);
+    }
+
+    // while
+    while a != 10 {
+        a += 1;
+        println!("while (a = {})", a);
+    }
+
+    println!("{}", a);
+    // loop
+
+    loop {
+        a += 1;
+        if a == 20 {
+            print!("{}", a);
+            break;
+        }
+    }
 }
