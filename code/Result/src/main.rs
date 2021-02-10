8c71f1180efbfdf9d8b86700e90a36ec9224af5d
RomChung <thelns@vip.qq.com>
2021-02-10 20:09:41 +0800

    fix: update

--- code/Result/src/main.rs
+++ code/Result/src/main.rs
@@ -1,13 +1,34 @@
-fn add(a: i64, b: i64) -> Result<i64, &'static str> {
-  if a > b {
-      return Err("error");
-  }
-  Ok(a + b)
+type UseResult = Result<i64, MathError>;
+
+#[derive(Debug)]
+enum MathError {
+    AddError,
+}
+
+fn add(a: i64, b: i64) -> UseResult {
+    if a > b {
+        return Err(MathError::AddError);
+    }
+    Ok(a + b)
+}
+
+fn use_add(a: i64, b: i64) -> UseResult {
+    let a = add(a, b)?;
+    Ok(a)
 }
 
 fn main() {
-  let a = 3i64;
-  let b = 2i64;
-  let r = add(a, b);
-  println!("{}", r.unwrap());
+    let a = 3i64;
+    let b = 2i64;
+    let r = use_add(a, b);
+    match r {
+        Ok(n) => {
+            println!("{}", n)
+        }
+        Err(e) => panic!(match e {
+            MathError::AddError => "add error",
+            _ => "other error",
+        }),
+    }
+    // println!("{}", r.unwrap());
 }
