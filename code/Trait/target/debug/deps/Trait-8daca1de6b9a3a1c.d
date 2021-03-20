3008a9ac293186df538e05aeb3525a04ccad8af8
RomChung <thelns@vip.qq.com>
2021-03-20 10:53:37 +0800

    feat: add Trait sample

--- code/Trait/target/debug/deps/Trait-8daca1de6b9a3a1c.d
+++
@@ -1,7 +0,0 @@
-/Users/romchung/Desktop/RustDoc/code/Trait/target/debug/deps/Trait-8daca1de6b9a3a1c.rmeta: src/main.rs src/person/mod.rs src/person/person.rs
-
-/Users/romchung/Desktop/RustDoc/code/Trait/target/debug/deps/Trait-8daca1de6b9a3a1c.d: src/main.rs src/person/mod.rs src/person/person.rs
-
-src/main.rs:
-src/person/mod.rs:
-src/person/person.rs:
--- code/Trait/target/debug/deps/Trait.d
+++
@@ -1,7 +0,0 @@
-/Users/romchung/Desktop/RustDoc/code/Trait/target/debug/deps/Trait: src/main.rs src/person/mod.rs src/person/person.rs
-
-/Users/romchung/Desktop/RustDoc/code/Trait/target/debug/deps/Trait.d: src/main.rs src/person/mod.rs src/person/person.rs
-
-src/main.rs:
-src/person/mod.rs:
-src/person/person.rs:
